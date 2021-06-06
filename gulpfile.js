96552108633f836ba3aaeb292f715f24533da860
boolean <y133@163.com>
2021-06-06 16:03:00 +0800

    配置打包iife和amd的配置

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
-const { src, dest } = require('gulp');
+const { src, dest,series } = require('gulp');
 const babel = require('gulp-babel');
 var uglify = require('gulp-uglify');
-const a=function (cb) {
+var browserify = require('gulp-browserify');
+const es6=function (cb) {
   src("./libs/index.js")
+  
         .pipe(babel({
           "presets": [
           [
@@ -15,9 +17,61 @@ const a=function (cb) {
           ]
         ]
         }))
-         .pipe(uglify())
-        .pipe(dest("./dist/"))
+        .pipe(browserify({}))
+        //  .pipe(uglify())
+        .pipe(dest("./dist/es/"))
+        //iife
   cb();
 }
-//编译js6到指定文件目录
-exports.default = a;
+
+const iife=function (cb) {
+  src("./libs/index.js")
+  
+        .pipe(babel({
+          "presets": [
+          [
+            "@babel/preset-env",
+            {
+              "useBuiltIns": "entry",//兼容，依赖core-js
+              "corejs": '3',
+              "targets": '> 1%,last 2 versions,not ie <= 11',
+            }
+          ],
+         
+        ],
+        "plugins": [['babel-plugin-transform-modules-iife', 
+              {
+                "exportNamespace":"disabledBodyScroll"
+             }
+            ]]
+        }))
+        .pipe(browserify({}))
+        //  .pipe(uglify())
+        .pipe(dest("./dist/iife/"))
+        //iife
+  cb();
+}
+const umd=function (cb) {
+  src("./libs/index.js")
+  
+        .pipe(babel({
+          "presets": [
+          [
+            "@babel/preset-env",
+            {
+              "useBuiltIns": false,//兼容，依赖core-js
+              "modules":"umd",
+              "corejs": '3',
+              "targets": '> 1%,last 2 versions,not ie <= 11',
+            }
+          ]
+        ]
+        }))
+        .pipe(browserify({}))
+        //  .pipe(uglify())
+        .pipe(dest("./dist/amd"))
+        //iife
+  cb();
+}
+
+exports.default = series(es6, umd,iife);
